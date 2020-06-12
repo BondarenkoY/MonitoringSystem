3a8d367b55cc4ce8febe68ff7b3194696da46006
BondarenkoY <>
2020-06-12 15:29:51 +0300

    created and filled in IUnitOfWork

---
+++ DAL/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAddedRepository added { get; }
+        ILocationRepository location { get; }
+        IOperatorRepository @operator { get; }
+        ISituationRepository situation { get; }
+        void Save();
+
+    }
+}
