250c9bbd19a9185b01974059a261fc91db5bf7cc
BondarenkoY <>
2020-06-12 14:51:04 +0300

    Filled in interfaces

---
+++ DAL/DAL/Repositories/Interfaces/ISituationRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+using DAL.Repositories.Interfaces;
+
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface ISituationRepository
+        : IRepository<situation>
+    {
+    }
+}
