eba2ed4fdc65c137353fffd13e5dab510e0cfc65
BondarenkoY <>
2020-06-12 15:08:48 +0300

    Added orderContext

---
+++ DAL/DAL/EF/orderContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace DAL.EF
+{
+    public class orderContext
+        : DbContext
+    {
+        public DbSet<added> Added { get; set; }
+        public DbSet<location> Location { get; set; }
+        public DbSet<@operator> Operator { get; set; }
+        public DbSet<situation> Situation { get; set; }
+        public orderContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
